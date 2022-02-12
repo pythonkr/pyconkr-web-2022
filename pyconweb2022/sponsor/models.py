769a4469575bd00abf6299f4078147f2fadfd2e4
Parkseongheum <ab6449@gmail.com>
2022-02-12 22:56:17 +0900

    다국어 지원 기능 삭제

--- pyconweb2022/sponsor/models.py
+++ pyconweb2022/sponsor/models.py
@@ -64,32 +64,32 @@ class Sponsor(models.Model):
 
     slug = models.SlugField(max_length=100, null=True, blank=True,
                             help_text='후원사 상세 페이지의 path로 사용됩니다.')
-    creator = models.ForeignKey(User, on_delete=models.CASCADE, help_text=_('후원사를 등록한 유저'),
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, help_text=('후원사를 등록한 유저'),
                                 related_name='sponsor_creator')
     name = models.CharField(max_length=255,
-                            help_text=_('후원사의 이름입니다. 서비스나 회사 이름이 될 수 있습니다.'))
+                            help_text=('후원사의 이름입니다. 서비스나 회사 이름이 될 수 있습니다.'))
     level = models.ForeignKey(SponsorLevel, null=True,
                               on_delete=models.SET_NULL, blank=True,
-                              help_text=_('후원을 원하시는 등급을 선택해주십시오. 모두 판매된 등급은 선택할 수 없습니다.'))
+                              help_text=('후원을 원하시는 등급을 선택해주십시오. 모두 판매된 등급은 선택할 수 없습니다.'))
     desc = models.TextField(null=True, blank=True,
-                            help_text=_('후원사 설명입니다. 이 설명은 홈페이지에 게시됩니다.'))
-    manager_name = models.CharField(max_length=100, help_text=_(
+                            help_text=('후원사 설명입니다. 이 설명은 홈페이지에 게시됩니다.'))
+    manager_name = models.CharField(max_length=100, help_text=(
         '준비위원회와 후원과 관련된 논의를 진행할 담당자의 이름을 입력해주십시오.'))
     manager_email = models.CharField(
-        max_length=100, help_text=_('입력하신 메일로 후원과 관련된 안내 메일이나 문의를 보낼 예정입니다. 후원 담당자의 이메일 주소를 입력해주십시오.'))
-    manager_id = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE, help_text=_('후원사를 위한 추가 아이디'),
+        max_length=100, help_text=('입력하신 메일로 후원과 관련된 안내 메일이나 문의를 보낼 예정입니다. 후원 담당자의 이메일 주소를 입력해주십시오.'))
+    manager_id = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE, help_text=('후원사를 위한 추가 아이디'),
                                    related_name='sponsor_temp_id')
     business_registration_number = models.CharField(max_length=100, null=True, blank=True,
-                                                    help_text=_('후원사 사업자 등록번호입니다. 세금 계산서 발급에 사용됩니다.'))
+                                                    help_text=('후원사 사업자 등록번호입니다. 세금 계산서 발급에 사용됩니다.'))
     business_registration_file = models.FileField(null=True, blank=True,
                                                   upload_to=registration_file_upload_to,
-                                                  help_text=_('후원사 사업자 등록증 스캔본입니다. 세금 계산서 발급에 사용됩니다.'))
+                                                  help_text=('후원사 사업자 등록증 스캔본입니다. 세금 계산서 발급에 사용됩니다.'))
     url = models.CharField(max_length=255, null=True, blank=True,
-                           help_text=_('파이콘 홈페이지에 공개되는 후원사 홈페이지 주소입니다.'))
+                           help_text=('파이콘 홈페이지에 공개되는 후원사 홈페이지 주소입니다.'))
     logo_image = SorlImageField(upload_to=logo_image_upload_to, null=True, blank=True,
-                                help_text=_('홈페이지에 공개되는 후원사 로고 이미지입니다.'))
+                                help_text=('홈페이지에 공개되는 후원사 로고 이미지입니다.'))
     virtual_booth_content = models.TextField(null=True, blank=True,
-                                             help_text=_('Virtual booth에 들어가는 내용입니다. 홈페이지의 virtual booth에 게시됩니다.'))
+                                             help_text=('Virtual booth에 들어가는 내용입니다. 홈페이지의 virtual booth에 게시됩니다.'))
     submitted = models.BooleanField(default=False,
                                     help_text='사용자가 제출했는지 여부를 저장합니다. 요청이 제출되면 준비위원회에서 검토하고 받아들일지를 결정합니다.')
     accepted = models.BooleanField(default=False,
