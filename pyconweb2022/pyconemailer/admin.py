2c66081b8a8611def12b0574694c19d711d95872
Parkseongheum <ab6449@gmail.com>
2022-03-22 23:26:42 +0900

    Mailing 모델을 Django Admin에 등록

--- pyconweb2022/pyconemailer/admin.py
+++ pyconweb2022/pyconemailer/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from pyconemailer.models import Mailing
+
+
 # Register your models here.
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ("category", "email")
+
+
+admin.site.register(Mailing, MailingAdmin)
