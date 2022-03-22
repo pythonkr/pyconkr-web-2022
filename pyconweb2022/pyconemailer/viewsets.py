55e7e18969018ae63dd051341d9ff0cbb4a029f0
Parkseongheum <ab6449@gmail.com>
2022-03-22 23:23:31 +0900

    api를 웹에서 호출할때 발생하는 오류 수정 빈 쿼리셋 반환시 오류발생 => 비어있는 데이터를 반환하도록 수정

--- pyconweb2022/pyconemailer/viewsets.py
+++ pyconweb2022/pyconemailer/viewsets.py
@@ -1,20 +1,21 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 
 from pyconemailer.models import Mailing
 from pyconemailer.serializers import MailingSerializer
 
 
 class MailingViewSet(ModelViewSet):
-    queryset = Mailing.objects.none()   # 개인정보 보호를 위해 조회기능 비활성화
+    queryset = Mailing.objects.none()  # 개인정보 보호를 위해 조회기능 비활성화
     serializer_class = MailingSerializer
 
     def list(self, request, *args, **kwargs):
         # 개인정보 보호를 위해 전체 조회(list) 액션 비활성화
-        queryset = Mailing.objects.none()
+        queryset = Mailing()
         serializer = MailingSerializer(queryset)
-        return serializer.data
+        return Response(serializer.data)
 
     def retrieve(self, request, *args, **kwargs):
-        queryset = Mailing.objects.none()
+        queryset = Mailing()
         serializer = MailingSerializer(queryset)
-        return serializer.data
+        return Response(serializer.data)
