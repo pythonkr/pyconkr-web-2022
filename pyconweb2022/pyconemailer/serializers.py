eae5a27754d56f5a76bc644ee49d827b175bbbd9
Parkseongheum <ab6449@gmail.com>
2022-03-22 23:02:39 +0900

    메일링 모델을 위한 serializer 작성 카테고리와 이메일만 전달받고, 나머지는 기본값(혹은 자동으로 설정되는 값) 사용

---
+++ pyconweb2022/pyconemailer/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework.serializers import ModelSerializer
+
+from pyconemailer.models import Mailing
+
+
+class MailingSerializer(ModelSerializer):
+    class Meta:
+        model = Mailing
+        fields = ["category", "email", ]
