c90e2a72730336fec7d6b2ad39b63c73551e9278
Parkseongheum <ab6449@gmail.com>
2022-04-24 22:48:21 +0900

    운영 배포를 위한 파이프라인 구성

--- pyconweb2022/pyconweb2022/settings.py
+++ pyconweb2022/pyconweb2022/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = "static/"
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
 AWS_S3_SESSION_PROFILE = "pycon"
-AWS_STORAGE_BUCKET_NAME = "pyconweb2022-static"
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STATIC_S3_BUCKET')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
