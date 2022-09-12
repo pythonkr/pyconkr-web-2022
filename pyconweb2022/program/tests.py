716c65cc03d2e3ca7df755e8855bbae08630738f
Parkseongheum <ab6449@gmail.com>
2022-09-12 20:44:25 +0900

    Test: 테스트 케이스 코드 롤백 (devdev)
    * Merge 중 발생한 코드 누락 수정

--- pyconweb2022/program/tests.py
+++ pyconweb2022/program/tests.py
@@ -6,57 +6,86 @@ from pyconweb2022 import config
 
 import datetime
 
-from django.contrib.auth import get_user_model
-from django.test import TestCase
-from rest_framework.test import APIRequestFactory
 
-from program.models import Proposal, ProgramCategory
-from program.viewsets import ProposalViewSet
-
-User = get_user_model()
-
-
-class ProgramTest(TestCase):
-    user_name = ["park", "seo", "kim", "jung", "kang"]
-    title = ["제목1", "제목2", "제목3", "제목4", "제목5"]
-    brief = ["brief1", "brief2", "brief3", "brief4", "brief5"]
-    desc = ["desc1", "desc2", "desc3", "desc4", "desc5"]
-    difficulty = ["B", "I", "E", "B", "I"]
-    duration = ["S", "L", "S", "L", "S"]
-
+class ProgramApiTest(TestCase):
     def setUp(self) -> None:
-        # 사용자 객체 생성
-        for i in range(len(self.user_name)):
-            new_user = User()
-            new_user.username = self.user_name[i]
-            new_user.set_password(self.user_name[i])
-            new_user.save()
+        user = User.objects.create()
+        user.username = "gdhong"
+        user.password = "password"
+        user.email = "gdhong@pycon.kr"
+        user.save()
 
-        # Category 생성
-        new_category = ProgramCategory()
-        new_category.name = ["카테고리"]
-        new_category.slug = ["category"]
-        new_category.save()
+        category = ProgramCategory(name="임시", slug="temp")
+        category.save()
 
-        # Proposal 생성
-        target_user = User.objects.get(username=self.user_name[0])
+        Proposal(
+            user=user,
+            user_name="홍길동",
+            title="세션 제목1",
+            brief="요약1",
+            desc="설명1",
+            comment="comment1",
+            difficulty="I",
+            duration="L",
+            language="K",
+            category=category,
+            accepted=True,
+            video_open_at=datetime.datetime.strptime(
+                config.PYCON_KR_2022_DATE1, "%Y%m%d"
+            ),
+        ).save()
 
-        for i in range(len(self.user_name)):
-            new_proposal = Proposal()
-            new_proposal.user = target_user
-            new_proposal.title = self.title[i]
-            new_proposal.brief = self.brief[i]
-            new_proposal.desc = self.desc[i]
-            new_proposal.difficulty = self.difficulty[i]
-            new_proposal.duration = self.duration[i]
-            new_proposal.category = new_category
+        Proposal(
+            user=user,
+            user_name="김철수",
+            title="세션 제목2",
+            brief="요약2",
+            desc="설명2",
+            comment="comment2",
+            difficulty="I",
+            duration="L",
+            language="K",
+            category=category,
+            accepted=True,
+            video_open_at=datetime.datetime.strptime(
+                config.PYCON_KR_2022_DATE2, "%Y%m%d"
+            ),
+        ).save()
 
-            new_proposal.save()
+        Proposal(
+            user=user,
+            user_name="badUser",
+            title="XXXXX",
+            brief="XXX",
+            desc="XXX",
+            comment="XXX",
+            difficulty="I",
+            duration="L",
+            language="K",
+            category=category,
+            accepted=False,
+            video_open_at=datetime.datetime.strptime(
+                config.PYCON_KR_2022_DATE2, "%Y%m%d"
+            ),
+        ).save()
 
-    def test_ProposalViewSet(self):
-        req_body = {}
+    def test_get_all_session(self):
+        c = Client()
+        response = c.get("/api/program/list/", {})
 
-        request = APIRequestFactory().get("/api/program/list", req_body)
-        response = ProposalViewSet().list(request=request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 2)
 
-        self.assertEqual(len(response.data), len(self.user_name))
+    def test_get_day1_session(self):
+        c = Client()
+        response = c.get("/api/program/list/day1/", {})
+
+        # self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
+
+    def test_get_day2_session(self):
+        c = Client()
+        response = c.get("/api/program/list/day2/", {})
+
+        # self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
