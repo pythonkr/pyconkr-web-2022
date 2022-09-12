19368ea4a06a7e2465198a8cf440ca07b9acb340
Parkseongheum <ab6449@gmail.com>
2022-09-12 18:41:02 +0900

    Update: import-export 모듈을 모든 앱에 추가 (emailer 제외)

--- pyconweb2022/content/admin.py
+++ pyconweb2022/content/admin.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.db import models
 
 from content.models import Content
+from content.resources import ContentResource
 
 from martor.widgets import AdminMartorWidget
+from import_export.admin import ImportExportModelAdmin
 
 
-class ContentAdmin(admin.ModelAdmin):
+class ContentAdmin(ImportExportModelAdmin):
+    resource_class = ContentResource
     list_display = (
         "slug",
         "title",
--- pyconweb2022/news/admin.py
+++ pyconweb2022/news/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
+from import_export.admin import ImportExportModelAdmin
+
 from news.models import News
+from news.resources import NewsResource
 
 
-class NewsAdmin(admin.ModelAdmin):
+class NewsAdmin(ImportExportModelAdmin):
+    resource_class = NewsResource
     app_label = "News"
     list_display = (
         "id",
