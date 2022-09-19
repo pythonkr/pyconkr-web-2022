c24fd780acf9b667fa3135ed7085d51cbeb59e86
Parkseongheum <ab6449@gmail.com>
2022-09-19 19:23:16 +0900

    Refactor: 코드 Linter 적용

--- pyconweb2022/program/migrations/0003_proposal_eng_desc.py
+++ pyconweb2022/program/migrations/0003_proposal_eng_desc.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('program', '0002_proposal_speaker_profile_img_alter_proposal_language'),
+        ("program", "0002_proposal_speaker_profile_img_alter_proposal_language"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='proposal',
-            name='eng_desc',
+            model_name="proposal",
+            name="eng_desc",
             field=models.TextField(blank=True, max_length=4000, null=True),
         ),
     ]
