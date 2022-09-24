ab16c53dd6e0323c1f77892990ff5ec606e2da5d
Parkseongheum <ab6449@gmail.com>
2022-09-24 16:42:41 +0900

    Update: 개인후원 모델을 Admin에 등록

--- pyconweb2022/sponsor/admin.py
+++ pyconweb2022/sponsor/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.db import models
 
-from sponsor.models import Sponsor, SponsorLevel
+from sponsor.models import Sponsor, SponsorLevel, PersonalSponsorship
 from sponsor.resources import SponsorResource
 
 from import_export.admin import ImportExportModelAdmin
@@ -71,6 +71,13 @@ class SponsorAdmin4Modusign(SponsorAdmin):
 
 # admin.site.register(SponsorProxyModel, SponsorAdmin4Modusign)
 
-class PersonalSponsorship(ImportExportModelAdmin):
-    list_display = ("name", "amount",)
+
+class PersonalSponsorshipAdmin(ImportExportModelAdmin):
+    list_display = (
+        "name",
+        "amount",
+    )
     ordering = ("-amount",)
+
+
+admin.site.register(PersonalSponsorship, PersonalSponsorshipAdmin)
