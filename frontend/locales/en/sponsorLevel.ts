48affa7579d1864905de76630f93e044369cd767
Parkseongheum <ab6449@gmail.com>
2022-09-26 23:44:04 +0900

    Update: 파트너A 등급명을 파트너로 변경 (영문)

--- frontend/locales/en/sponsorLevel.ts
+++ frontend/locales/en/sponsorLevel.ts
@@ -2,7 +2,7 @@ import { SponsorLevel } from '../../data/enums/SponsorLevel'
 
 export default {
     [SponsorLevel.LEVEL_1]: 'Keystone',
-    [SponsorLevel.LEVEL_2]: 'Partner A',
+    [SponsorLevel.LEVEL_2]: 'Partner',
     [SponsorLevel.LEVEL_3]: 'Partner B',
     [SponsorLevel.LEVEL_4]: 'Startup',
     [SponsorLevel.LEVEL_5]: 'Community'
