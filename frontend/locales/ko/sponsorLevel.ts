b4b042cc33c2f43b76d5ceb7940b95255fabd458
Parkseongheum <ab6449@gmail.com>
2022-09-26 23:29:46 +0900

    후원사 등급 파트너A의 명칭을 파트너로 변경

--- frontend/locales/ko/sponsorLevel.ts
+++ frontend/locales/ko/sponsorLevel.ts
@@ -2,7 +2,7 @@ import { SponsorLevel } from '../../data/enums/SponsorLevel'
 
 export default {
     [SponsorLevel.LEVEL_1]: '키스톤',
-    [SponsorLevel.LEVEL_2]: '파트너A',
+    [SponsorLevel.LEVEL_2]: '파트너',
     [SponsorLevel.LEVEL_3]: '파트너B',
     [SponsorLevel.LEVEL_4]: '스타트업',
     [SponsorLevel.LEVEL_5]: '커뮤니티'
