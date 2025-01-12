d6606d9e0e381d00145c30017773f886c3510670
MUsoftware <musoftware@daum.net>
2025-01-12 15:56:57 +0900

    PSQL 지원 추가

--- pyconweb2022/pyconweb2022/settings_prod.py
+++ pyconweb2022/pyconweb2022/settings_prod.py
@@ -3,16 +3,28 @@ import os
 from pyconweb2022.settings import *
 
 # RDS
-DATABASES = {
-    "default": {
-        "ENGINE": "mysql.connector.django",
-        "NAME": os.getenv("AWS_RDS_DATABASE"),
-        "USER": os.getenv("AWS_RDS_USER_ID"),
-        "PASSWORD": os.getenv("AWS_RDS_PW"),
-        "HOST": os.getenv("AWS_RDS_HOST"),
-        "PORT": os.getenv("AWS_RDS_PORT"),
+if os.environ.get("AWS_PSQL_HOST"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "HOST": os.getenv("AWS_PSQL_HOST"),
+            "PORT": os.getenv("AWS_PSQL_PORT"),
+            "NAME": os.getenv("AWS_PSQL_DATABASE"),
+            "USER": os.getenv("AWS_PSQL_USER_ID"),
+            "PASSWORD": os.getenv("AWS_PSQL_PW"),
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "mysql.connector.django",
+            "NAME": os.getenv("AWS_RDS_DATABASE"),
+            "USER": os.getenv("AWS_RDS_USER_ID"),
+            "PASSWORD": os.getenv("AWS_RDS_PW"),
+            "HOST": os.getenv("AWS_RDS_HOST"),
+            "PORT": os.getenv("AWS_RDS_PORT"),
+        }
+    }
 
 # S3 static
 del AWS_S3_SESSION_PROFILE  # 운영환경에서는 Key와 Secret을 사용해 S3 인증 수행
